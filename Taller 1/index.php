e5767acd178c2ef5201e9e7b5111de0a6e0dadc6
Luis Miguel Ra <luismiguelraaaa@gmail.com>
2024-04-07 18:36:46 -0500

    conexion

--- Taller 1/index.php
+++ Taller 1/index.php
@@ -1,3 +1,21 @@
 <?php
 
-echo"hola mundo";
+try{
+    $mbd = new PDO('mysql:host=localhost;dbname=taller', "root", "admin");
+
+    $sql = "SELECT * FROM usuarios";
+
+
+    $res = $mbd->query($sql);
+
+    $respuesta = [];
+    foreach ($res as $fila) {
+        $respuesta[] = $fila;
+    }
+
+    header('content-type:application/json;charset=utf-8');
+    echo json_encode($respuesta);
+    $mbd = null;
+} catch(PDOException $e) {
+    print('error mi dev'. $e->getMessage());
+    die();
